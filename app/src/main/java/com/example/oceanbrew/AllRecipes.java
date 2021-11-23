7a04b84b25c9da32ed676ee132bdce5a058c894d
197PM09534 - Lê Đức Anh Tuấn - K25T-PM <tuan.197pm09534@vanlanguni.vn>
2021-11-24 02:22:47 +0700

    Layout update part 7

--- app/src/main/java/com/example/oceanbrew/AllRecipes.java
+++ app/src/main/java/com/example/oceanbrew/AllRecipes.java
@@ -3,6 +3,7 @@ package com.example.oceanbrew;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class AllRecipes extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class AllRecipes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_recipes);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
     }
 }

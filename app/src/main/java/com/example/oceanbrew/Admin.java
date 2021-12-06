1ac942835a5367cf7d3bc5e89cc513c15d86c8e5
197PM09534 - Lê Đức Anh Tuấn - K25T-PM <tuan.197pm09534@vanlanguni.vn>
2021-12-06 22:52:22 +0700

    6/12/2021 10:51 PM

--- app/src/main/java/com/example/oceanbrew/Admin.java
+++ app/src/main/java/com/example/oceanbrew/Admin.java
@@ -48,13 +48,7 @@ public class Admin extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarAdmin.toolbar);
-        binding.appBarAdmin.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
 
